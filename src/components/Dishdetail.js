40777ab2c15007e461616ea422a0e0d99cfa9c88
gsakshay <gsakshay20002002@gmail.com>
2020-05-30 00:44:21 +0530

    changed components as functional components

--- src/components/Dishdetail.js
+++ src/components/Dishdetail.js
@@ -1,24 +1,16 @@
-import React,{Component} from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
-class Dishdetail extends Component{
-	constructor(props){
-		super (props);
-		this.state={
-
-		}
-	}
-
-	render(){
-
-			const comments = this.props.dish.comments.map((comment)=>{
+function Dishdetail(props) {
+	const comments = props.dish.comments.map((comment)=>{
 						if(comment!==null){
 							return(
 								<ListGroupItem className="col">
 							        <ListGroupItemHeading>{comment.comment}</ListGroupItemHeading>
 							        <ListGroupItemText>
-							        {comment.author}{comment.date}
+							        <p>{comment.author}</p>
+							        <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
 						        	</ListGroupItemText>
 						        </ListGroupItem>
 								);
@@ -31,16 +23,16 @@ class Dishdetail extends Component{
 			
 
 
-			console.log(this.props);
+			console.log(props);
             return(
             	<div className="row">
             	
-                  {(this.props.dish!==null)?
+                  {(props.dish!==null)?
                   	(<Card className="col">
-                    <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                    <CardImg top src={props.dish.image} alt={props.dish.name} />
                     <CardBody>
-                      <CardTitle>{this.props.dish.name}</CardTitle>
-                      <CardText>{this.props.dish.description}</CardText>
+                      <CardTitle>{props.dish.name}</CardTitle>
+                      <CardText>{props.dish.description}</CardText>
                     </CardBody>
                   </Card>):
                   
@@ -60,7 +52,9 @@ class Dishdetail extends Component{
             );
         
 	}
-}
+	
+	
 
+	
 export default Dishdetail;
 
